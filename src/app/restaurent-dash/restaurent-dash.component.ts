5af4d06041e796e72c4c11cb662938aaf42d4d9c
Md Eesha <md.eesha@chicmic.co.in>
2022-06-14 15:57:03 +0530

    completed post and delete api

--- src/app/restaurent-dash/restaurent-dash.component.ts
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Form } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurentData } from './restaurent.model';
 
 @Component({
   selector: 'app-restaurent-dash',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurentDashComponent implements OnInit {
 
-  constructor() { }
+  formValue !: FormGroup
+  restaurentModelObj:RestaurentData = new RestaurentData
+  allRestaurentData:any
+
+  constructor(private fb: FormBuilder, private api:ApiService) { }
+
 
   ngOnInit(): void {
+    this.formValue = this.fb.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      service: ['']
+    })
+    this.getAllData()
+  }
+
+  //Now Subscribing our data which is mapped via services..
+  addRestaurent(){
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.mobile = this.formValue.value.mobile;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.service = this.formValue.value.service;
+
+    this.api.postRestaurent(this.restaurentModelObj).subscribe(res=>{
+      console.log(res)
+      alert('Restaurent Data Added Successfully')
+      this.getAllData()
+      this.formValue.reset()      
+
+    },
+    err=>{
+      alert('Something went wrong!!!!!')
+    }
+    )
+
+  }
+
+  //get all data
+  getAllData(){
+    this.api.getRestaurent().subscribe(res=>{
+      this.allRestaurentData=res;
+    
+      console.log(this.allRestaurentData)
+    })
+  }
+
+  //delete data
+  deletetRestaurent(data:any){
+    console.log('data for delete Restaurent Record==>',data)
+    this.api.deletetRestaurent(data.id).subscribe(res=>{
+      alert('Restaurent Record Deleted Successfully')
+      this.getAllData();
+    })
   }
 
 }
